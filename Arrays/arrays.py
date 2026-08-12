93b325a5e25bc4bacb3b152c48ec2dca1a673bb8
Katta Rajashekar <rajashekarkatta18@gmail.com>
2026-08-12 17:27:02 +0530

    Explained detail about the time complexity of operations

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -35,5 +35,41 @@ s.reverse_traversal(arr)
 
 """
 
+# Insert Element at the Beginning of an Array
+"""Inserting an element at the beginning of an array takes O(n) time in a normal array. suppose we want to insert an element in the beginning of an array
+So The existing elements have to move one position to the right. We need to move n elements, so This takes Time complexity O(n).
+# Example
+arr = [2,3,4,5]
+arr.insert(0, 1)
+print(arr)  # [1,2,3,4,5]
 
+"""
 
+# Insert Element at a Given Position in an Array
+"""To add an element at a given position in an array, shift all the elements from that position one index to the right, 
+and after shifting insert the new element at the required position. So this take Time Complexity O(n)
+# Example
+arr = [1,2,3,4,5]
+arr.insert(2, 10)
+print(arr)
+"""
+
+# Insert Element at the End of an Array
+"""To insert an element at the end of an array, we can simply add the new element at the nth index. So this take Time Complexity O(1)
+# Example
+arr = [1,2,3,4]
+arr.append(5)
+print(arr)
+"""
+
+# Delete an Element from the Beginning of an Array
+"""The idea is to start from the second element and shift all the elements one position to the left. After shifting all the elements,
+reduce the array size by 1 to remove the extra element at the end.So this take Time Complexity O(n)
+"""
+
+# Delete an Element from a Given Position in an Array
+"""The idea is to shift all the elements occurring after the given position, one index to the left and reduce the size of the array by 1.
+So this take Time Complexity O(n)
+"""
+
+# 
