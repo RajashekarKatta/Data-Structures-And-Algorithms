25e9c1c7a0c5d4981ff2c28521fb8f9df13c3432
Katta Rajashekar <rajashekarkatta18@gmail.com>
2026-08-12 17:00:29 +0530

    Explained about array and trversal types detail

--- Arrays/arrays.py
+++ Arrays/arrays.py
@@ -1,53 +1,39 @@
 # Arrays
-# An Array is a collection of similar types of elements stored in a comtigous memory location. we can access the item through array index numbers
+# Array is a Linear Data Structure. An Array is a Collection of similar types of elements stored in a contiguous memory location.we ca acess the each item through array index
 
+# Operations On Array
+# Traversal In Array --> we have two types of Traversal
+"""1.Linear Traversal: Linear Traversal is the process of visiting each element of an array sequentially.
+starting from the first element and moving towards to the last element.During this process each element is printed onr after the another. 
+in this order stored in an array
 
-
-
-# Baic Array Problems
-print("__" * 30)
-
-# Alternate elements of an array
+# Example
 class Solution:
-    def alter_native_elements(self, arr):
-        result = []
-        for i in range(0, len(arr), 2):
-            result.append(arr[i])
-        return result
-
-s = Solution()
-arr = [1, 2, 3, 4, 5, 6]
-print(s.alter_native_elements(arr))
-
-
-print("\n\n\n")
-
-
-
-# # Leaders in an array
-# Given an array arr[] of size n, the task is to find all the Leaders in the array. An element is a Leader if it is greater than or equal to all the elements to its right side.
-# Note: The rightmost element is always a leader.
-# Input: arr[] = [16, 17, 4, 3, 5, 2]
-# Output: [17 5 2]
-
-class Solution:
-    def leaders(self, arr):
-        leaders = []
-        max_from_right = arr[-1]
-        leaders.append(max_from_right)
+    def traversal(self, arr):
         for num in arr:
-            if num > max_from_right:
-                max_from_right = num
-                leaders.append(num)
-        leaders.reverse()
-        return leaders
+            print(num)
 
 s = Solution()
-arr = [16, 17, 4, 3, 5, 2]
-print(s.leaders(arr))
+arr = [1,2,3,4,5]
+s.traversal(arr)
+""" 
 
+# Reverse Traversal
+"""Reverse traversal is the process of visiting each element of an array starting from the last element and moving towards the first element.
+This method is useful when you need to process the elements of an array in reverse order. 
+In this type of traversal, you begin from the last index (the rightmost element) and work your way to the first index (the leftmost element).
 
-print("\n\n\n")
+# Example
+class Solution:
+    def reverse_traversal(self, arr):
+        for i in range(len(arr)-1, -1, -1):
+            print(arr[i])
+
+s = Solution()
+arr = [1,2,3,4,5]
+s.reverse_traversal(arr)   
+
+"""
 
 
 
