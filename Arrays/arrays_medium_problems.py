fa277011b03324fa82853859a2776bf41405fc11
Katta Rajashekar <rajashekarkatta18@gmail.com>
2026-08-16 16:26:36 +0530

    learned rearrange problem and practiced

--- Arrays/arrays_medium_problems.py
+++ Arrays/arrays_medium_problems.py
@@ -1,4 +1,6 @@
 # Array medium problems or you can say Level tWo problems in array
+
+
 # # Duplicate within K Distance in an Array
 # Given an integer array arr[] and an integer k, determine whether there exist two indices i and j such that arr[i] == arr[j] and |i - j| ≤ k. 
 #If such a pair exists, return 'Yes', otherwise return 'No'.
@@ -15,3 +17,23 @@ class SOlution:
 s = SOlution()
 arr = [1, 2, 3, 1, 4, 5]
 print(s.duplicate_within_k_distance(arr, 3))
+
+
+# Rearrange array such that even positioned are greater than odd
+# Given an array arr[], rearrange its elements according to 1-based indexing such that for every even index i, arr[i] is greater than or equal to arr[i-1],
+# and for every odd index i, arr[i] is less than or equal to arr[i-1]. Return the rearranged array that satisfies these conditions for all valid indices.
+# Find the resultant array.[consider 1-based indexing].
+# Input: arr[] = [1, 2, 2, 1]  ->  Output: [2 1 2] 1
+class Solution:
+    def rearrange_arr(self, arr):   # Alternative sorting  problem
+        for i in range(1, len(arr)):
+            if (1 + i) % 2 == 0:
+                if arr[i] > arr[i-1]:
+                    arr[i] , arr[i-1] = arr[i-1], arr[i]
+            else:
+                if arr[i] < arr[i-1]:
+                    arr[i], arr[i-1] = arr[i-1], arr[i]
+        return arr
+s = Solution()
+arr = [1, 2, 2, 1] 
+print(s.rearrange_arr(arr))                    
